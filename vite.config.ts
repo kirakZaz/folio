67735b8985d028107a3864771531f51a2c76e75f
Kira Zakirov <kirka.zaz@gmail.com>
2026-03-17 16:41:13 +1100

    deployment_

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
